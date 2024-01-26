32a91cd501f83ec63dc9391c418a926164e2186f
Nahuewe <nahuelsoriap@gmail.com>
2024-01-26 12:56:11 -0300

    ultimos cambios

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
+// Función para ocultar el contenedor de carga y mostrar el contenido principal cuando todas las imágenes se hayan cargado
+function hideLoading() {
+    document.getElementById('loading-container').style.display = 'none';
+    document.querySelector('main').style.display = 'block';
+}
 
+// Evento que se activa cuando todas las imágenes se han cargado
+window.addEventListener('load', hideLoading);
